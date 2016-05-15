a9788e011de83f21966a16aa9347ec7ca656f5ef
Moritz Eissenhauer <moritz.eissenhauer@gmail.com>
2016-05-15 19:49:12 +0200

    improve time measuring

--- parsers.cpp
+++ parsers.cpp
@@ -1,10 +1,12 @@
 //DON'T MOVE//
-#define MAXNUMBERS 1000000
+#define MAXNUMBERS 100000
 #define MAXDIGITS 10
 #define MAXCHARACTERS MAXNUMBERS * MAXDIGITS + MAXDIGITS
 //////////////
 
 #include <iostream>
+#include <fstream>
+#include <chrono>
 
 template<typename T, char Delimiter>
 inline T toUnsignedIntegral(char*& position) 
@@ -20,18 +22,61 @@ inline T toUnsignedIntegral(char*& position)
 	return result;
 }
 
+template<typename T, char Delimiter, size_t Buffersize>
+inline T nextNumber(char*& position, char*& inputBuffer) 
+{
+	T result = 0;
+	while (*position != Delimiter)
+	{
+		if (position == &inputBuffer[Buffersize])
+		{
+			position = inputBuffer;
+			fread(inputBuffer, sizeof(char), Buffersize, stdin);
+		}
+		result *= 10;
+		result += *position - 48;
+		++position;
+	}
+	++position;
+	return result;
+}
+
+
+//Code for Testing
 char input[MAXCHARACTERS];
 uint32_t numbers[MAXNUMBERS];
-int main()
-{
-	uint32_t charCount = fread(input, sizeof(char), MAXCHARACTERS, stdin);
 
+void write(uint32_t* numbers, uint32_t count, char* filename)
+{
+	std::ofstream output(filename);
+	output << count << '\n';
+	for (uint32_t i = 0; i < count; ++i)
+	{
+		output << numbers[i] << '\n';
+	}
+	output.close();
+}
+
+uint32_t readUsingToUnsignedIntegral()
+{
+	fread(input, sizeof(char), MAXCHARACTERS, stdin);
 	char *it = input;
 	uint32_t numberCount = toUnsignedIntegral<uint32_t, '\n'>(it);
 	for (uint32_t i = 0; i < numberCount; ++i)
 	{
 		numbers[i] = toUnsignedIntegral<uint32_t, '\n'>(it);
 	}
+	return numberCount;
+}
+
+int main()
+{
+	auto begin = std::chrono::high_resolution_clock::now();
+	auto count = readUsingToUnsignedIntegral();
+	auto end = std::chrono::high_resolution_clock::now();
+	auto deltaTimeInMs = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+	std::cout << "toUnsignedIntegral:" << deltaTimeInMs << '\n';
+	write(numbers, count, "out_toUnsignedIntegral");
 
     return 0;
 }
--- timer.py
+++ timer.py
@@ -2,11 +2,13 @@ import random
 import subprocess
 import time
 import math
+import filecmp
 
 INPUT = 'input'
 CPP_FILE = 'parsers.cpp'
 PROGRAM = 'build/Release/numberparsing'
-SAMPLESIZE = 1000
+SAMPLESIZE = 100
+OUT_PREFIX = 'out_'
 
 def create_input(max_numbers, max_digits):
     with open(INPUT, 'w', newline='\n') as input_file:
@@ -28,37 +30,65 @@ def read_input_length():
     return (max_numbers, max_digits)
 
 def time_program():
-    start_time = 0
-    end_time = 0
+    times = []
     with open(INPUT, 'r') as input_file:
         start_time = time.time()
-        process = subprocess.Popen(PROGRAM, stdin=input_file)
+        process = subprocess.Popen(PROGRAM, stdin=input_file,
+            stdout=subprocess.PIPE)
         process.wait()
-        end_time = time.time()
-    return (end_time - start_time) * 1000
+        for line in process.stdout:
+            if len(line) == 0:
+                continue
+            output = line.decode('utf-8').rstrip('\r\n')
+            name_and_time = output.split(':')
+            times.append((name_and_time[0], int(name_and_time[1])))
+    return times
+
+def calculate_average_and_sd(times):
+    average = sum(times) / len(times)
+    square_deviations = [(t - average) ** 2 for t in times]
+    sd = math.sqrt(sum(square_deviations) / len(times))
+    return (average, sd)
+
+def calculate_results(times):
+    sorted_times = {}
+    for name, time in times:
+        if name in sorted_times:
+            sorted_times[name].append(time)
+        else:
+            sorted_times[name] = [time]
+    result = {}
+    for name in sorted_times:
+        result[name] = calculate_average_and_sd(sorted_times[name])
+    return result
+
+def compare_file_to_input(name):
+    with open(INPUT, 'r') as in_file, open(OUT_PREFIX + name, 'r') as out_file:
+        if in_file.readline().rstrip('\r\n') != out_file.readline().rstrip('\r\n'):
+            return False
+    return True
+
+def verify_results(names):
+    result = {}
+    for name in names:
+        result[name] = compare_file_to_input(name)
+    return result
 
 def main():
+    times = []
     random.seed(0)
     max_numbers, max_digits = read_input_length()
     print('Creating input file ({} numbers, {} digits each).'.format(
         max_numbers, max_digits))
     create_input(max_numbers, max_digits)
-    times = []
     for i in range(SAMPLESIZE):
         print(' ' * 20, end='\r')
         print('Run {}/{}'.format(i + 1, SAMPLESIZE), end='\r')
-        times.append(time_program())
+        times.extend(time_program())
     print()
-    average_time = sum(times) / len(times)
-    absolute_deviations = [abs(t - average_time) for t in times]
-    average_absolute_deviation = (sum(absolute_deviations) / len(times))
-    deviation_percentage = average_absolute_deviation / average_time * 100
-    standard_deviation = sum([d ** 2 for d in absolute_deviations]) / len(times)
-    standard_deviation = math.sqrt(standard_deviation)
-    print('avg: {:.2f}ms'.format(average_time))
-    print('avg abs. deviation: {:.2f}ms ({:.2f}%)'.format(
-        average_absolute_deviation, deviation_percentage))
-    print('SD: {:.2f}ms'.format(standard_deviation))
+    results = calculate_results(times)
+    correct = verify_results(results.keys())
+    print(correct)
 
 if __name__ == '__main__':
     main()
